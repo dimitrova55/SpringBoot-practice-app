f69708a1eb321550a4fc68147112f7bc153c2a78
Dilyana Dimitrova <dilyanadimitrova55@gmail.com>
2025-01-08 20:39:19 +0900

    Spring Security: JDBC Authentication

---
+++ src/main/resources/sql/insert-values.sql
@@ -0,0 +1,13 @@
+INSERT INTO users VALUES
+('john','{noop}test123',true),
+('mary','{noop}test123',true),
+('susan','{noop}test123',true);
+
+
+INSERT INTO authorities VALUES
+('john','ROLE_STUDENT'),
+('mary','ROLE_STUDENT'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_STUDENT'),
+('susan','ROLE_MANAGER'),
+('susan','ROLE_ADMIN');
